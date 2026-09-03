c2575ee72fc6763b1eb39f2b00b106d3e4f1e966
“Sazu97” <sara97rm@gmail.com>
2026-09-03 14:02:23 +0200

    feat: solve exercise 1

--- script.js
+++ script.js
@@ -11,10 +11,18 @@ console.log('Hola soy tu consola y juntas vamos a aprender Javascript')
 //Ejercicio 1: Escribe un condicional if/else que imprima en la consola 'Eres mayor de edad' cuando la constante age tenga el valor correspondiente, si no es así, debe imprimir 'No eres aún mayor de edad'
 
 const age = 18
+
 //Escribe tu código aquí
+
+if (age >= 18) {
+    console.log('Eres mayor de edad')
+} else {
+    console.log('No eres aún mayor de edad')
+}
 
 
 //Ejercicio 2: Cambia el valor de age a 12 para mirar el resultado del ejercicio anterior.
+
 
 
 //Ejercicio 3: Escribe un condicional if/else que lea la constante pet y si es un perro, que imprima en la consola "Tengo un perro", si es un gato, que imprima en la consola "Tengo un gato", si es un pájaro, que imprima en la consola "Tengo un pájaro" y si no es ninguno de los 3 que imprima "No tengo una mascota convencional"
